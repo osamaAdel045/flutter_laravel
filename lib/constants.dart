dc0a4b61fa9689904f6874c61153a24a472c0bca
Ahmed3mad <3umduck@gmail.com>
2023-01-22 13:35:21 +0200

    noti webhook

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,6 +3,6 @@ import 'package:flutter/material.dart';
 const FORGE_TOKEN_KEY = 'forge_token';
 final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 
-const serverNotificationsBaseUrl = 'http://10.0.0.2:1337';
+const serverNotificationsBaseUrl = 'https://us-central1-laravel-forge-ac6ae.cloudfunctions.net/app';
 const serverNotificationsWebHook = '$serverNotificationsBaseUrl/new_deployment';
 const serverNotificationsFCMApi = '$serverNotificationsBaseUrl/fcm_token';
